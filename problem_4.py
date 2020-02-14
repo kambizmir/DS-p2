c1cb9b01ea4471ffd0374b3c9eae7a111ad6bef7
kmir <kmir@paciolan.com>
2020-02-14 08:19:19 -0800

    problem 4 solution

--- problem_4.py
+++ problem_4.py
@@ -20,6 +20,48 @@ class Group(object):
         return self.name
 
 
+
+
+user_cache = {}
+root_group = None
+
+
+def search_groups_for_user(user, group, parent_name_set):
+
+    if user not in user_cache:
+            user_cache[user] = set() 
+   
+    set_copy = parent_name_set.copy()
+    set_copy.add(group.name)
+
+    if user in group.get_users(): 
+        user_cache[user] = user_cache[user].union(set_copy)
+
+    for g in group.get_groups():
+        search_groups_for_user(user, g, set_copy)
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+
+    if user not in user_cache:
+        search_groups_for_user(user, root_group, set())
+
+    if group.name in user_cache[user]:            
+        return True
+    else:
+        return False
+    
+    return False
+
+
+# Test case 1
 parent = Group("parent")
 child = Group("child")
 sub_child = Group("subchild")
@@ -31,18 +73,100 @@ child.add_group(sub_child)
 parent.add_group(child)
 
 
+# Test case 2
+g1 = Group("g1")
+g2 = Group("g2")
+g3 = Group("g3")
+g4 = Group("g4")
+g5 = Group("g5")
+g6 = Group("g6")
+g7 = Group("g7")
+g8 = Group("g8")
 
-global_cache = {}
+u1 = "u1"
+u2 = "u2"
+u3 = "u3"
+u4 = "u4"
 
-def search_groups_for_user(user, top_group):
-    pass
+g1.add_group(g2)
+g1.add_group(g3)
+g1.add_group(g4)
 
-def is_user_in_group(user, group):
-    """
-    Return True if user is in the group, False otherwise.
+g2.add_group(g5)
+g2.add_group(g6)
 
-    Args:
-      user(str): user name/id
-      group(class:Group): group to check user membership against
-    """
-    return None
+g3.add_group(g7)
+
+g6.add_group(g8)
+
+g1.add_user(u1)
+
+g2.add_user(u2)
+g2.add_user(u3)
+
+g3.add_user(u2)
+
+g4.add_user(u2)
+
+g7.add_user(u2)
+g7.add_user(u3)
+
+g8.add_user(u4)
+
+
+##########    
+print("Test 1")
+user_cache = {}
+root_group = parent
+
+
+print(is_user_in_group(sub_child_user, parent))
+print(user_cache)
+
+#########
+user_cache = {}
+root_group = g1
+
+print("\nTest 2")
+print(is_user_in_group(u1, g1))
+print(is_user_in_group(u1, g2))
+print(is_user_in_group(u1, g3))
+print(is_user_in_group(u1, g4))
+print(is_user_in_group(u1, g5))
+print(is_user_in_group(u1, g6))
+print(is_user_in_group(u1, g7))
+print(is_user_in_group(u1, g8))
+
+print("\nTest 3")
+print(is_user_in_group(u2, g1))
+print(is_user_in_group(u2, g2))
+print(is_user_in_group(u2, g3))
+print(is_user_in_group(u2, g4))
+print(is_user_in_group(u2, g5))
+print(is_user_in_group(u2, g6))
+print(is_user_in_group(u2, g7))
+print(is_user_in_group(u2, g8))
+
+print("\nTest 4")
+print(is_user_in_group(u3, g1))
+print(is_user_in_group(u3, g2))
+print(is_user_in_group(u3, g3))
+print(is_user_in_group(u3, g4))
+print(is_user_in_group(u3, g5))
+print(is_user_in_group(u3, g6))
+print(is_user_in_group(u3, g7))
+print(is_user_in_group(u3, g8))
+
+print("\nTest 5")
+print(is_user_in_group(u4, g1))
+print(is_user_in_group(u4, g2))
+print(is_user_in_group(u4, g3))
+print(is_user_in_group(u4, g4))
+print(is_user_in_group(u4, g5))
+print(is_user_in_group(u4, g6))
+print(is_user_in_group(u4, g7))
+print(is_user_in_group(u4, g8))
+
+print("\ncache")
+print(user_cache)
+
