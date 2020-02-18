d5558cd6f822d2c1b5d7efb966938e0fa1681318
kmir <kmir@paciolan.com>
2020-02-17 23:09:30 -0800

    problem 3 implementation, not complete yet...

--- problem_3.py
+++ problem_3.py
@@ -1,7 +1,73 @@
 import sys
+from queue import PriorityQueue
+
+class TreeNode:
+     def __init__(self):
+        self.data = None
+        self.frequency = 0
+        self.left = None
+        self.right = None
+
+     def __lt__(self,other):
+        return self.frequency < other.frequency
+
+def preorder(node):
+    print(node.data,node.frequency)
+    if node.left:
+    	preorder(node.left)
+    if node.right:
+    	preorder(node.right)
+
 
 def huffman_encoding(data):
-    pass
+    frequency_hash = {}
+    pq = PriorityQueue()
+
+    for c in data:
+        if c not in frequency_hash:
+            frequency_hash[c] = 1
+        frequency_hash[c] += 1
+
+    frequency_tuples = [(k, v) for k, v in frequency_hash.items()] 
+    #sorted_tuples = sorted(frequency_tuples , key=lambda x: x[1])
+
+    for t in frequency_tuples:
+        node = TreeNode()
+        node.data = t[0]
+        node.frequency = t[1]
+        pq.put( node )
+
+    root = None
+    while not pq.empty():
+        if pq.qsize() == 1:
+            root = pq.get()
+            break
+        x1 = pq.get()
+        x2 = pq.get()
+        t = TreeNode()
+        t.frequency = x1.frequency + x2.frequency
+        t.left = x1
+        t.right = x2
+        pq.put(t)
+
+
+
+    preorder(root)
+
+    #print("AAA",root.frequency, root.left.frequency, root.right.frequency)
+
+    #print (pq.qsize())
+
+
+    # while not pq.empty():
+    #     next_item = pq.get()
+    #     print(next_item.frequency,next_item.data)
+
+
+
+    #print (sorted_tuples)
+
+
 
 def huffman_decoding(data,tree):
     pass
