75b3ffc31ea94bd4f7dc5c83e9f921543ef7e734
kmir <kmir@paciolan.com>
2020-03-12 15:45:03 -0700

    Add more test for problem 2

--- problem_2.py
+++ problem_2.py
@@ -20,35 +20,59 @@ def find_files(suffix, path):
 
     
     result = []
-    for file in os.listdir(path):        
-        if os.path.isfile(os.path.join(path,file)) and file.endswith(suffix):
-            #print(file)
-            result.append(os.path.join(path,file))
-        if os.path.isdir(os.path.join(path,file)):
-            result.extend ( find_files(suffix, os.path.join(path,file)) )
+    try:
+        
+        for file in os.listdir(path):        
+            if os.path.isfile(os.path.join(path,file)) and file.endswith(suffix):            
+                result.append(os.path.join(path,file))
+            if os.path.isdir(os.path.join(path,file)):
+                result.extend ( find_files(suffix, os.path.join(path,file)) )
+
+    except Exception as e:
+        print(str(e))
 
     return result
 
+
+print("Happy path tests:\n")
 print(".c files:")
-print( find_files(".c","./testdir") ,"\n")
+print( find_files(".c","./testdir") )
 
 print(".h files:")
-print( find_files(".h","./testdir") ,"\n")
+print( find_files(".h","./testdir") )
 
 print(".c files from top directory")
-print( find_files(".c",".") ,"\n")
+print( find_files(".c","."))
 
 print(".c files from sub directory 1")
-print( find_files(".c","./testdir/subdir1") ,"\n")
+print( find_files(".c","./testdir/subdir1") )
 
 print(".c files from sub directory 2")
-print( find_files(".c","./testdir/subdir2") ,"\n")
+print( find_files(".c","./testdir/subdir2"))
 
 print(".c files from sub directory 3")
-print( find_files(".c","./testdir/subdir3") ,"\n")
+print( find_files(".c","./testdir/subdir3"))
 
 print(".c files from sub directory 4")
-print( find_files(".c","./testdir/subdir4") ,"\n")
+print( find_files(".c","./testdir/subdir4"))
 
 print(".c files from sub directory 5")
-print( find_files(".c","./testdir/subdir5") ,"\n")
+print( find_files(".c","./testdir/subdir5") )
+
+print("\nEdge case: nonexisting suffix, result should be empty list\n")
+
+print(".x files:")
+print( find_files(".x","./testdir") ,"\n")
+
+
+print("\nEdge case: nonexisting path, should show graceful message and empty result\n")
+
+print(".c files:")
+print( find_files(".c","./testdir1") ,"\n")
+
+
+
+print("\nEdge case: nonexisting suffix, nonexisting path, should show graceful message and empty result\n")
+
+print(".pdf files:")
+print( find_files(".y","./testdir2") ,"\n")
